9cb2f2847ceeb0641542dc0257c3d39dcf677dd6
jpsantosz <jpsantosc1503@gmail.com>
2025-04-29 15:55:40 -0300

    setting main function

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # importando as classes criadas
 from tarefa import Tarefa
 from escalonador import Escalonador
+
+def main():
+    # Cria 4 tarefas
+    tarefas = [
+        Tarefa("Tarefa 1"),
+        Tarefa("Tarefa 2"),
+        Tarefa("Tarefa 3"),
+        Tarefa("Tarefa 4")
+    ]
+    
+# Cria o escalonador com quantum de 4 segundos
+    escalonador = Escalonador(tarefas, quantum=4)
+
+if __name__ == "__main__":
+    main()
