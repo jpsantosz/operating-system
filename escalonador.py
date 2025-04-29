fc603df33fd274a6e4d8ecba1cc124a424965afd
jpsantosz <jpsantosc1503@gmail.com>
2025-04-29 10:38:53 -0300

    iniciando função de escrita no arquivo de log

--- escalonador.py
+++ escalonador.py
@@ -10,3 +10,8 @@ class Escalonador:
         self.quantum = quantum
         self.arquivo_log = arquivo_log
         self._iniciar_log()
+        
+# criando a função para iniciar a escrita no arquivo de log
+    def _iniciar_log(self):
+        with open(self.log_file, "w") as f:
+            f.write("Log de execução do escalonador\n")
